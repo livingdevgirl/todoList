6e5b63e2aead715314b1aa9158f3de413dbc1660
Emma Dean <emmadeannow@gmail.com>
2017-08-02 14:44:43 -0400

    added the index selector to correctly select each of the peices in the data set and used params in another get request in order to push to a new array with each item clicked, also changed it to an ANCHOR tag instead of a button since that causes issues again

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const expressHandlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
-const info = [];
+
 const fs = require('fs');
 const app = express()
 
@@ -15,16 +15,50 @@ app.use(bodyParser.urlencoded({extended:false}));
 
 
 app.get("/", function (req, res) {
+  //everything we get is on the homepage, we define each key value pair with the dataset listed here, and we are selecting the homepage
   res.render('home', {
-    data: info
-})
+    //render the view of home
+    //this is the data source
+    data: info,
+    doneList: completed
+  })
 });
+
+const info = [];
+const completed = [];
 
 app.post("/", function(req, res){
   let todoItem = req.body.todoItem
   info.push(todoItem);
+  console.log(todoItem);
+  console.log(info);
   res.redirect("/")
 });
+
+
+// app.get('/', function(req, res){
+//   res.render('home', {data: completed})
+// });
+//
+app.get("/done/:index", function(req, res){
+
+  //we are just getting from a new page for the brief moment that runs these parameters
+
+  let done = info.splice(req.params.index,1);
+//splice separates each individual peice of the array and takes starting index, how many to remove, and what to replace it with
+
+  completed.push(done);
+
+  //here we are pushing to our empty array and using the done (as listed in our dataset (inital get))
+  console.log(completed);
+  res.redirect("/");
+  //since we just used the route for this parameter process, we want to redirect to the homepage and display all info after
+  //using :/index and /done, was just a peice of the process. @ index is the ref in the view
+
+});
+
+
+
 
 app.use(express.static('public'));
 
